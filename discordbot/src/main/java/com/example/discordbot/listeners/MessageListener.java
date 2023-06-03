5031fe41732694bd62a45e3df8db7ab8eb2c0b42
cwurlnd <69475242+cwurlnd@users.noreply.github.com>
2023-06-02 20:02:51 -0400

    added game decision

--- discordbot/src/main/java/com/example/discordbot/listeners/MessageListener.java
+++ discordbot/src/main/java/com/example/discordbot/listeners/MessageListener.java
@@ -1,11 +1,15 @@
 package com.example.discordbot.listeners;
 
+import java.util.Random;
+
 import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
 
 public abstract class MessageListener {
 
-    private String author = "UNKNOWN";
+    private String[] games = { "CS:GO", "Dead by Daylight", "GTFO", "Sea of Thieves", "Phasmophobia" };
+    private int randomNum = 0;
+    private String chosenGame = "UNKNOWN";
 
     public Mono<Void> processMessage(final Message eventMessage) {
         return Mono.just(eventMessage)
@@ -13,13 +17,18 @@ public abstract class MessageListener {
                     final Boolean isNotBot = message.getAuthor()
                             .map(user -> !user.isBot())
                             .orElse(false);
-                    if (isNotBot) {
-                        message.getAuthor().ifPresent(user -> author = user.getUsername());
-                    }
                     return isNotBot;
                 })
+                .filter(message -> {
+                    final String myMessage = message.getContent();
+                    Random generator = new Random();
+                    randomNum = generator.nextInt(games.length);
+                    chosenGame = games[randomNum];
+                    return myMessage.equalsIgnoreCase("!choosegame");
+                })
                 .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage(String.format("Hello '%s'", author)))
+                .flatMap(channel -> channel
+                        .createMessage(String.format("The game I have chosen is ... %s!", chosenGame)))
                 .then();
     }
 }
